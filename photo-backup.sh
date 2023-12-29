5a5cce1cd656bf85919af55a70b30cb722477f6c
Jure Merhar <dev@merhar.si>
2023-12-29 20:56:46 +0100

    back up from two disks

--- photo-backup.sh
+++ photo-backup.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 LOG=/var/log/photo-backup.log
-DIR=/Volumes/PhotoStore
-CAF=/usr/bin/caffeinate
+SRC1=/Volumes/PhotoStore
+SRC2=/Volumes/MorePhotos
+TEMP=/tmp
+
 HOST=`aurora`
 ssh -q $HOST-local exit
 [[ $? -eq 0 ]] && HOST=$HOST-local
-echo BEGIN `date` >> $LOG
-echo 'Deleting .DS_Store files' >> $LOG
-$CAF find $DIR -name '.DS_Store' >> $LOG
-$CAF find $DIR -name '.DS_Store' -delete >> $LOG
-echo 'Deleting *_original files' >> $LOG
-$CAF find $DIR -name '*_original' >> $LOG
-$CAF find $DIR -name '*_original' -delete >> $LOG
-$CAF -s dot_clean -v $DIR >> $LOG 2>> $LOG
-echo Backing up photos to $HOST >> $LOG
-$CAF -s /usr/bin/rsync -aHv --progress --exclude '.*' --delete $DIR/ $HOST:/mnt/storage/photos >> $LOG
-echo END `date` >> $LOG
+DEST="$HOST:/mnt/storage/photos"
+
+run () {
+    "$@" | tee -a $LOG
+}
+
+remove () {
+    run echo "Deleting $2 files from $1"
+    run find $1 -name "$2" -delete -print    
+}
+
+clean () {
+    remove "$1" '.DS_Store'
+    remove "$1" '*_original'
+    run dot_clean -v $1
+}
+
+backup () {
+    run echo Generating an exclusion list for $2
+    EXCL="$TEMP/exclusion-list.csv"
+    find $2/* -print0 | xargs -0 realpath --relative-to=$2 > $EXCL
+
+    run echo Backing up $1 to $HOST
+    run rsync -aHv --progress --exclude '.*' --exclude-from=$EXCL --delete "$1/" "$DEST"
+
+    rm $EXCL
+}
+
+run echo BEGIN `date`
+clean $SRC1
+clean $SRC2
+backup $SRC1 $SRC2
+backup $SRC2 $SRC1
+run echo END `date`
